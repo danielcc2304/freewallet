9a9257f4f2e6daebef07d00c49d792e8bf289cde
Daniel Carrasco de la Chica <daniel.carrasco@nter.es>
2026-02-17 17:03:26 +0100

    subir version

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -152,7 +152,7 @@ export function Settings() {
                     <div className="settings__about">
                         <div className="about-item">
                             <Info size={16} />
-                            <span>FreeWallet v2.3.1</span>
+                            <span>FreeWallet v2.3.2</span>
                         </div>
                         <p className="about-description">
                             Aplicación de gestión de portfolio de inversiones.

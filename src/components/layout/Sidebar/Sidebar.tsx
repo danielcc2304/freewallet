cbce52cd92e0d2e1ff4d3dd604ef4c1ed9da0481
Daniel Carrasco de la Chica <daniel.carrasco@nter.es>
2026-03-10 18:25:04 +0100

    subir version

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -195,7 +195,7 @@ export function Sidebar({ isOpen, onToggle }: SidebarProps) {
                     <Link to="/terms" className="sidebar__terms-link" onClick={closeMobileSidebar}>
                         Terminos y condiciones
                     </Link>
-                    <div className="sidebar__version">v3.5.0</div>
+                    <div className="sidebar__version">v3.5.1</div>
                 </div>
             </aside>
 
